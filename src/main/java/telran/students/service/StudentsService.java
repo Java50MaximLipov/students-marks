fc30266397ca2c3f4f7a5314af0b9a568f97978f
mlipov <maxim.lipov@gmail.com>
2024-02-20 18:13:49 +0200

    240220 Fix: updated interface and TODO's of the appropriate methods.

--- src/main/java/telran/students/service/StudentsService.java
+++ src/main/java/telran/students/service/StudentsService.java
@@ -78,7 +78,7 @@ public interface StudentsService {
 
 	List<Mark> getStudentMarksAtDates(long id, LocalDate from, LocalDate to);
 
-	List<String> getBestStudents(int nStudents);
+	List<Long> getBestStudents(int nStudents);
 
-	List<String> getWorstStudents(int nStudents);
+	List<Long> getWorstStudents(int nStudents);
 }
